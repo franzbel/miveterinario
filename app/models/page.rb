3ee4448bad217c5b74f84e53f8cdc9fe5eb8ae9d
franzbel <franzbeltran79@hotmail.com>
2015-08-31 13:54:21 -0400

    fix magazine

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ActiveRecord::Base
   belongs_to :magazine
-  has_attached_file :avatar, :styles => {:large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => {:large => "565x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
