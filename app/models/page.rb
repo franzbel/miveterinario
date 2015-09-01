75abdf5069489ad557fd8cbe0075b20c37d2e752
franzbel <franzbeltran79@hotmail.com>
2015-09-01 18:04:24 -0400

    dropbox

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ActiveRecord::Base
   belongs_to :magazine
   has_attached_file :avatar, 
   					:storage => :dropbox,
-  					:dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+  					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
   					:styles => {:large => "565x600>", :medium => "300x300>", :thumb => "100x100>" }, 
   					:default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
--- app/models/preview.rb
+++ app/models/preview.rb
@@ -1,7 +1,7 @@
 class Preview < ActiveRecord::Base
   has_attached_file :avatar, 
   					:storage => :dropbox,
-  					:dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+  					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
   					:styles => { :medium => "300x300>",	:thumb => "100x100>" }, 
   					:default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
